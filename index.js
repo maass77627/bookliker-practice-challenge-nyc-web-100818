a6bcdda38a2691dd9c6caebddffae76ad304c035
Kelly Maass <kmaass77627@gmail.com>
2026-03-15 15:26:19 -0500

    done

--- index.js
+++ index.js
@@ -1,69 +1,80 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let list = document.getElementById("list-panel")
+     let showPanel = document.getElementById("show-panel")
+     let ul = document.createElement("ul")
+
+fetch("http://localhost:3000/books")
+.then((response) => response.json())
+.then((json) => {
+    console.log(json)
+    let books = json
+    books.forEach((book) => renderBooks(book))
+    
+})
+.catch((error) => console.error(error.message))
 
 
 
-let bookList = document.getElementById('list');
 
+function renderBooks(book) {
+    console.log(book)
+    let li = document.createElement("li")
+    li.textContent = book.title
+    li.addEventListener("click", () => {displayBookInfo(book)})
+    list.appendChild(li)
+}
 
-    fetch('http://localhost:3000/books')
-    .then((response) => response.json())
-    .then((books) => {
-        console.log(books);
-        books.forEach( book => 
-        renderBooks(book)
-        )
-
+function displayBookInfo(book) {
+    showPanel.innerHTML = ""
+    let image = document.createElement("img")
+    image.src = book.img_url
+    let description = document.createElement("p")
+    description.textContent = book.description
+    // let ul = document.createElement("ul")
+    let button = document.createElement("button")
+    button.textContent = "like"
+    button.addEventListener("click", () => {addLikes(book)})
+    
+    book.users.forEach((user) => {
+        let li = document.createElement("li")
+        li.textContent = user.username
+        ul.appendChild(li)
     })
+    showPanel.appendChild(image)
+    showPanel.appendChild(description)
+    showPanel.appendChild(ul)
+    showPanel.appendChild(button)
 
-    function renderBooks(book) {
-        let li = document.createElement('li');
-        li.addEventListener('click', () => { showBookDetails(book)})
-        li.innerHTML = book.title;
-        bookList.appendChild(li);
-    }
+}
 
-    function showBookDetails(book) {
-        let showPanel = document.getElementById('show-panel');
-        showPanel.innerHTML = '';
-        let title = document.createElement('h2')
-        title.innerHTML = book.title;
-        let img = document.createElement('img');
-        img.src = book.img_url;
-        let description = document.createElement('p')
-        description.innerHTML = book.description;
+function addLikes(book) {
+    console.log(book)
+    let id = book.id
+    let currentUser = {id: 1, username: "pouros"};
 
-        showPanel.appendChild(title);
-        showPanel.appendChild(img);
-        showPanel.appendChild(description);
+    fetch(`http://localhost:3000/books/${id}`, {
+        method: "PATCH",
+        headers: {
+           "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            {users: [...book.users, currentUser]}
+        )
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        let li = document.createElement("li")
+        let num = json.users.length - 1
+        li.textContent = json.users[num].username
+        ul.appendChild(li)
+        console.log(json)})
 
-        let userList = document.createElement('ul');
-        book.users.forEach( user => {
-            let userLi = document.createElement('li');
-            userLi.innerHTML = user.username;
-            userList.appendChild(userLi);
-        })
-        showPanel.appendChild(userList);
-        let likeButton = document.createElement('button');
-        likeButton.innerHTML = "Like";
-        likeButton.addEventListener('click', () => {
-            let currentUser = {id: 1, username: "pouros"};
-            fetch(`http://localhost:3000/books/${book.id}`, {
-                method: 'PATCH',
-                headers: { "Content-Type": "application/json"},
-                body: JSON.stringify({users: [...book.users, currentUser]})
-            })
-            .then( response => response.json())
-            .then( json => console.log(json));
-            // let hasLiked = false;
-        });
-        showPanel.appendChild(likeButton);
+}
 
 
-    }
+})       
 
 
-    // let bookList = document.getElementById('list');
-    // bookList
 
-})        
+
 
